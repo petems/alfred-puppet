8d95afe0f143d6e8a3d40548e2c5e9b79aa4f39b
NicoPaez <nicopaez@gmail.com>
2015-08-27 14:24:15 -0300

    aptget update added

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,18 @@
 class alfred ($db_user = 'alfred', $db_pass = 'mypassword', $config_branch = 'master', $ssh_key = 'undefined') {
 
+	class apt_update {
+  	exec { 'do_apt_update':
+    	command     => '/usr/bin/apt-get update',
+    	timeout     => 300,
+  	}
+	}
+
+	stage { 'init': before  => Stage['main'] }
+
+	class { 'apt_update': 
+  	stage => init
+	}
+
 	class { 'alfred::foundation': }
 
 	class { 'alfred::ruby': }
