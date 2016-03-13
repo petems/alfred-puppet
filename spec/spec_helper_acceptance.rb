559a3d4909ce5d8a32b685d63992f141a8a866f5
Nicolas Paez <nicopaez@gmail.com>
2016-03-13 12:11:14 -0300

    Test fixed

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,20 +3,22 @@ require 'pry'
 
 hosts.each do |host|
   # Install Puppet
-  on host, install_puppet
+  install_puppet_on host
 end
 
 RSpec.configure do |c|
   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
+  
   c.formatter = :documentation
 
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module
     puppet_module_install(:source => module_root, :module_name => 'nicopaez-alfred')
-    hosts.each do |host|
-      on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-    end
+
+    #hosts.each do |host|
+      #install_dev_puppet_module_on(host, :source => module_root, :module_name => 'nicopaez-alfred',:target_module_path => '/etc/puppet/modules')
+    #  on host, puppet('module','install','puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
+    #end
   end
 end
